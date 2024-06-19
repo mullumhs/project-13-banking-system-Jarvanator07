1d9094f90356d41b92a01a35139b6346046d994b
Jarvanator07 <140124249+Jarvanator07@users.noreply.github.com>
2024-06-19 10:38:11 +1000

    zsdzsdfzsdfzsdf

--- account.py
+++ account.py
@@ -11,39 +11,36 @@ class Account:
         self.set_account_balance(balance)
 
     def get_account_name(self):
-        return self.name
+        return self.__name
 
     def set_account_name(self, name):
-        if isinstance(self, Account):
-            self.name = name
-        else:
-            raise TypeError('account must be an Account object')
+            if isinstance(name, str):
+                self.__name = name
+            else:
+                raise TypeError('Name must be a string')
 
     def get_account_ID(self):
-        return self.ID
+        return self.__ID
 
     def set_account_ID(self, ID):
-        if isinstance(self, Account):
             if isinstance(ID, (int, float)):
-                self.ID = ID
+                self.__ID = ID
             else:
                 raise TypeError('ID must be a number')
-        else:
-            raise TypeError('account must be an Account object')
     
     def get_account_balance(self):
-        return "Balance: ${:.2f}".format(self.balance) 
+        return "Balance: ${:.2f}".format(self.__balance) 
     
     def set_account_balance(self, balance):
         if isinstance(balance, (int, float)) and balance >= 0:
-            self.balance = balance
+            self.__balance = balance
         else:
             raise ValueError("Balance must be a non-negative number")
         
 if __name__ == '__main__':
     try:
         account1 = Account('John', 123456789, 1000.00)
-        print("success", account1.get_account_name(account1), account1.get_account_ID(account1), account1.get_account_balance(account1))
+        print("success", account1.get_account_name(), account1.get_account_ID(), account1.get_account_balance())
     except TypeError as e:
         print("error", e)
 
