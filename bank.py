5ebe85e21b4e32d40eeb4373c4953bcd7f7406a7
Jarvanator07 <140124249+Jarvanator07@users.noreply.github.com>
2024-06-24 09:51:05 +1000

    efdfgg

--- bank.py
+++ bank.py
@@ -4,3 +4,37 @@
 # Author: 
 # Date: 
 """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+from account import Account
+
+class Bank:
+    def __init__(self):
+        self.accounts = []
+
+    def add_account(self, account):
+        if isinstance(account, Account):
+            self.accounts.append(account)
+        else:
+            raise TypeError("Account must be an instance of the Account class")
+        
+    def remove_account(self, account): 
+        if isinstance(account, Account):
+            self.accounts.remove(account)
+        else:
+            raise TypeError("Account must be an instance of the Account class")
+        
+    def update_account(self, account):
+        if isinstance(account, Account):
+            for i in range(len(self.accounts)):
+                if self.accounts[i].get_account_ID() == account.get_account_ID():
+                    self.accounts[i] = account
+                    break
+                if i == len(self.accounts) - 1:
+                    raise ValueError("Account not found")
+        else:
+            raise TypeError("Account must be an instance of the Account class")
+
+    def display_accounts(self):
+        if len(self.accounts) == 0:
+            print("No accounts to display")
+        else:
+            print("Accounts:")
--- main.py
+++ main.py
@@ -4,3 +4,47 @@
 # Author: 
 # Date: 
 """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+from account import Account
+from bank import Bank
+
+def main():
+    accounts = []
+    bank = Bank()
+    
+    while True:
+        print("1. Add account")
+        print("2. Remove account")
+        print("3. Update account")
+        print("4. Display accounts")
+        print("5. Exit")
+        
+        choice = int(input("Enter your choice: "))
+        
+        if choice == 1:
+            account_number = int(input("Enter account number: "))
+            name = input("Enter name: ")
+            balance = float(input("Enter balance: "))
+            account = Account(account_number, name, balance)
+            bank.add_account(account)
+            
+        elif choice == 2:
+            account_number = int(input("Enter account number to remove: "))
+            bank.remove_account(account_number)
+                
+        elif choice == 3:
+            account_number = int(input("Enter account number to update: "))
+            name = input("Enter new name: ")
+            balance = float(input("Enter new balance: "))
+            bank.update_account(account_number, name, balance)
+                
+        elif choice == 4:
+            bank.display_accounts()
+                
+        elif choice == 5:
+            break
+            
+        else:
+            print("Invalid choice")
+
+if __name__ == "__main__":
+    main()
